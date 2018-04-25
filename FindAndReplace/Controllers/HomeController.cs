aea551123bfd6a8f5e15c88d263012f56cc97798
Guest User <Guest@epicodus-27.local>
2018-04-25 15:48:03 -0700

    add views and controllers

---
+++ FindAndReplace/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Replacement.Models;
+using System;
+
+namespace Replacement.Controllers
+{
+  public class HomeController : Controller
+  {
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    [HttpPost("/replacement")]
+    public ActionResult Check()
+    {
+      FindNReplace inputValues = new FindNReplace(Request.Form["inputSentence"], Request.Form["wordToFind"], Request.Form["replaceWord"]);
+      string newSentece = inputValues.ToReplace();
+      Console.WriteLine(newSentece);
+      return View("Result", newSentece);
+
+    }
+
+  }
+}
